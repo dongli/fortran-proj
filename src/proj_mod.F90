b04340369fe7fff04e55399802a3bc2fa94d1e62
Li Dong <dongli@lasg.iap.ac.cn>
2020-02-10 12:25:16 +0800

    Fix string with ending null char
    Also add r4 interfaces.

--- src/proj_mod.F90
+++ src/proj_mod.F90
@@ -21,15 +21,19 @@ module proj_mod
     character(:), allocatable :: dst_crs
   contains
     procedure :: init => proj_init
-    procedure :: set_src_crs => proj_set_src_crs
-    procedure :: set_dst_crs => proj_set_dst_crs
-    procedure :: transform => proj_transform
+    procedure, private :: proj_transform_r4, proj_transform_r8
+    generic :: transform => proj_transform_r4, proj_transform_r8
     final :: proj_final
   end type proj_type
 
   type, bind(c) :: PJ_COORD
     real(c_double) v(4)
   end type PJ_COORD
+
+  interface lcc_crs
+    module procedure lcc_crs_r4
+    module procedure lcc_crs_r8
+  end interface lcc_crs
 
   interface
     type(c_ptr) function proj_context_create() bind(c)
@@ -82,7 +86,7 @@ module proj_mod
     end function proj_errno_string
 
     integer(c_int) function strlen(str) bind(c)
-      import
+      use, intrinsic :: iso_c_binding
       type(c_ptr), value :: str
     end function strlen
   end interface
@@ -92,32 +96,23 @@ contains
   subroutine proj_init(this, src_crs, dst_crs)
 
     class(proj_type), intent(out) :: this
-    character(*), intent(in), optional :: src_crs
-    character(*), intent(in), optional :: dst_crs
+    character(*), intent(in) :: src_crs
+    character(*), intent(in) :: dst_crs
 
     this%ctx = proj_context_create()
-    if (present(src_crs)) this%src_crs = src_crs
-    if (present(dst_crs)) this%dst_crs = dst_crs
+    this%src_crs = trim(src_crs) // c_null_char
+    this%dst_crs = trim(dst_crs) // c_null_char
+
+    this%pj = proj_create_crs_to_crs(this%ctx, this%src_crs, this%dst_crs, c_null_ptr)
+    if (proj_errno(this%pj) /= 0) then
+      write(*, *) '[Error]: Failed to create pj object!'
+      write(*, *) trim(this%src_crs)
+      write(*, *) trim(this%dst_crs)
+      write(*, *) get_proj_error_message(proj_errno(this%pj))
+      stop 1
+    end if
 
   end subroutine proj_init
-
-  subroutine proj_set_src_crs(this, crs)
-
-    class(proj_type), intent(inout) :: this
-    character(*), intent(in) :: crs
-
-    this%src_crs = crs
-
-  end subroutine proj_set_src_crs
-
-  subroutine proj_set_dst_crs(this, crs)
-
-    class(proj_type), intent(inout) :: this
-    character(*), intent(in) :: crs
-
-    this%dst_crs = crs
-
-  end subroutine proj_set_dst_crs
 
   function latlon_crs(ellps) result(res)
 
@@ -136,7 +131,54 @@ contains
 
   end function latlon_crs
 
-  function lcc_crs(lat_0, lon_0, lat_1, lat_2, x_0, y_0, ellps) result(res)
+  function lcc_crs_r4(lat_0, lon_0, lat_1, lat_2, x_0, y_0, ellps) result(res)
+
+    real(4), intent(in) :: lat_0
+    real(4), intent(in) :: lon_0
+    real(4), intent(in) :: lat_1
+    real(4), intent(in) :: lat_2
+    real(4), intent(in), optional :: x_0
+    real(4), intent(in), optional :: y_0
+    character(*), intent(in), optional :: ellps
+    character(256) res
+
+    character(10) lat_0_s, lon_0_s, lat_1_s, lat_2_s
+    character(20) x_0_s, y_0_s
+    character(256) ellps_
+
+    if (present(x_0)) then
+      write(x_0_s, '(F20.5)') x_0
+    else
+      x_0_s = '0.0'
+    end if
+    if (present(y_0)) then
+      write(y_0_s, '(F20.5)') y_0
+    else
+      y_0_s = '0.0'
+    end if
+    if (present(ellps)) then
+      ellps_ = ellps
+    else
+      ellps_ = 'sphere'
+    end if
+
+    write(lat_0_s, '(F10.5)') lat_0
+    write(lon_0_s, '(F10.5)') lon_0
+    write(lat_1_s, '(F10.5)') lat_1
+    write(lat_2_s, '(F10.5)') lat_2
+
+    write(res, '("+proj=lcc +lat_0=", A, " +lon_0=", A, " +lat_1=", A, " +lat_2=", A, " +x_0=", A, " +y_0=", A, " +ellps=", A)') &
+      trim(adjustl(lat_0_s)), &
+      trim(adjustl(lon_0_s)), &
+      trim(adjustl(lat_1_s)), &
+      trim(adjustl(lat_2_s)), &
+      trim(adjustl(x_0_s)), &
+      trim(adjustl(y_0_s)), &
+      trim(ellps_)
+
+  end function lcc_crs_r4
+
+  function lcc_crs_r8(lat_0, lon_0, lat_1, lat_2, x_0, y_0, ellps) result(res)
 
     real(8), intent(in) :: lat_0
     real(8), intent(in) :: lon_0
@@ -181,9 +223,25 @@ contains
       trim(adjustl(y_0_s)), &
       trim(ellps_)
 
-  end function lcc_crs
+  end function lcc_crs_r8
 
-  subroutine proj_transform(this, xi, yi, xo, yo)
+  subroutine proj_transform_r4(this, xi, yi, xo, yo)
+
+    class(proj_type), intent(inout) :: this
+    real(4), intent(in) :: xi
+    real(4), intent(in) :: yi
+    real(4), intent(out) :: xo
+    real(4), intent(out) :: yo
+
+    real(8) x8, y8
+
+    call this%transform(dble(xi), dble(yi), x8, y8)
+    xo = x8
+    yo = y8
+
+  end subroutine proj_transform_r4
+
+  subroutine proj_transform_r8(this, xi, yi, xo, yo)
 
     class(proj_type), intent(inout) :: this
     real(8), intent(in) :: xi
@@ -194,14 +252,8 @@ contains
     type(PJ_COORD) pj_xi, pj_xo
 
     if (.not. c_associated(this%pj)) then
-      this%pj = proj_create_crs_to_crs(this%ctx, this%src_crs, this%dst_crs, c_null_ptr)
-      if (proj_errno(this%pj) /= 0) then
-        write(*, *) '[Error]: Failed to create pj object!'
-        write(*, *) trim(this%src_crs)
-        write(*, *) trim(this%dst_crs)
-        write(*, *) get_proj_error_message(proj_errno(this%pj))
-        stop 1
-      end if
+      write(*, *) '[Error]: Projection object is not initialized!'
+      stop 1
     end if
 
     pj_xi = proj_coord(xi, yi, 0.0d0, 0.0d0)
@@ -209,7 +261,7 @@ contains
     xo = pj_xo%v(1)
     yo = pj_xo%v(2)
 
-  end subroutine proj_transform
+  end subroutine proj_transform_r8
 
   subroutine proj_final(this)
 
